0812d91ebea0a40deeb17939ff26732f85167515
hollowpointer <hollowpointer@pm.me>
2025-08-21 00:32:45 +0200

    removed interface tests as mocking physical ones is too complex for now

--- src/net/interface.rs
+++ src/net/interface.rs
@@ -1,5 +1,6 @@
 use pnet::datalink::NetworkInterface;
 use crate::cmd::Target;
+use std::path::Path;
 
 pub fn select(target: Target, interfaces: &[NetworkInterface]) -> Option<NetworkInterface> {
     match target {
@@ -12,12 +13,28 @@ fn select_lan(interfaces: &[NetworkInterface]) -> Option<NetworkInterface> {
     interfaces
         .iter()
         .find(|i|
-            i.is_up()
+                i.is_up()
                 && i.mac.is_some()
                 && i.is_broadcast()
+                && is_physical(i)
                 && !i.is_loopback()
                 && !i.is_point_to_point()
                 && i.ips.iter().any(|ip| ip.is_ipv4())
         )
         .cloned()
+}
+
+#[cfg(target_os = "linux")]
+fn is_physical(interface: &NetworkInterface) -> bool {
+    Path::new(&format!("/sys/class/net/{}/device", interface.name)).exists()
+}
+
+#[cfg(target_os = "windows")]
+fn is_physical(interface: &NetworkInterface) -> bool {
+    true
+}
+
+#[cfg(target_os = "macos")]
+fn is_physical(interface: &NetworkInterface) -> bool {
+    true
 }
