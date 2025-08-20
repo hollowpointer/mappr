1d94b9017b316733b078570aa1dd3cea4aae87dd
hollowpointer <hollowpointer@pm.me>
2025-08-21 00:43:27 +0200

    LAN interface selection now prefers wired over wireless

--- src/net/interface.rs
+++ src/net/interface.rs
@@ -10,20 +10,27 @@ pub fn select(target: Target, interfaces: &[NetworkInterface]) -> Option<Network
 
 // Selects the first LAN interface it finds
 fn select_lan(interfaces: &[NetworkInterface]) -> Option<NetworkInterface> {
-    interfaces
+    let mut candidates: Vec<_> = interfaces
         .iter()
-        .find(|i|
-                i.is_up()
+        .filter(|i| {
+            i.is_up()
                 && i.mac.is_some()
                 && i.is_broadcast()
                 && is_physical(i)
                 && !i.is_loopback()
                 && !i.is_point_to_point()
                 && i.ips.iter().any(|ip| ip.is_ipv4())
-        )
-        .cloned()
+        })
+        .collect();
+
+    // Prefer wired over wireless
+    candidates.sort_by_key(|k| is_wireless(*k));
+    candidates.first().cloned().cloned()
 }
 
+/*********************************
+OS dependent functions for PHYSICAL
+**********************************/
 #[cfg(target_os = "linux")]
 fn is_physical(interface: &NetworkInterface) -> bool {
     Path::new(&format!("/sys/class/net/{}/device", interface.name)).exists()
@@ -37,4 +44,12 @@ fn is_physical(interface: &NetworkInterface) -> bool {
 #[cfg(target_os = "macos")]
 fn is_physical(interface: &NetworkInterface) -> bool {
     true
+}
+
+/*********************************
+OS dependent functions for WIRELESS
+**********************************/
+#[cfg(target_os = "linux")]
+fn is_wireless(interface: &NetworkInterface) -> bool {
+    Path::new(&format!("sys/class/net/{}/wireless", interface)).exists()
 }
