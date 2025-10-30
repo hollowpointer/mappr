7fd033f30cbed53d7d34e8a3a65587764ab32123
hollowpointer <hollowpointer@pm.me>
2025-10-30 13:45:41 +0100

    added basic linux firewall detectability

--- src/cmd/info/firewall.rs
+++ src/cmd/info/firewall.rs
@@ -4,19 +4,38 @@ use anyhow;
 pub fn print_firewall_status() -> anyhow::Result<()> {
     use std::process::Command;
     use colored::Colorize;
-    use crate::utils::print;
+    use crate::utils::{colors, print};
 
     print::header("firewall status");
+
+    let mut has_ufw: bool = false;
+    let mut has_firwalld: bool = false;
 
     let ufw_output = Command::new("ufw")
     .arg("status")
     .output();
-    let ufw_value = match ufw_output {
-        Ok(_) => "active".green().bold(),
+    let ufw_activity = match ufw_output {
+        Ok(_) => { has_ufw = true; "active".green().bold() },
         Err(_) => "inactive".red().bold()
     };
 
-    print::aligned_line("UFW", ufw_value);
+    let firewalld_output = Command::new("firewall-cmd")
+    .arg("--state")
+    .output();
+    let firewalld_activity = match firewalld_output {
+        Ok(_) => { has_firwalld = true; "active".green().bold() },
+        Err(_) => "inactive".red().bold()
+    };
+
+    print::aligned_line("ufw", ufw_activity);
+    print::aligned_line("firewalld", firewalld_activity);
+    
+    if !has_ufw && !has_firwalld {
+        let output = format!("{}", 
+        "No active firewall detected. Services may be exposed to public.".color(colors::TEXT_DEFAULT));
+        print::println("");
+        print::println(&output);
+    }
 
     Ok(())
 }
